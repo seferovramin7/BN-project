7b2e44908840e0d77612209be3e3b824f2ded5af
seferovramin7 <seferovramin7@gmail.com>
2022-02-11 15:31:10 +0400

    test controller added

--- src/main/java/com/autoauction/buynow/controller/InfoController.java
+++ src/main/java/com/autoauction/buynow/controller/InfoController.java
@@ -3,10 +3,15 @@ package com.autoauction.buynow.controller;
 import com.autoauction.buynow.model.CarTypeModel;
 import com.autoauction.buynow.repository.CarTypeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.net.URI;
+import java.net.URISyntaxException;
 
 @RestController
 @RequestMapping("api")
@@ -23,8 +28,15 @@ public class InfoController {
     }
 
     @GetMapping(value = "/alive")
-    public String getAlive() {
+    public String getAlive() throws URISyntaxException {
+        RestTemplate restTemplate = new RestTemplate();
+        System.setProperty("https.proxyHost", "10.0.3.18");
+        System.setProperty("https.proxyPort", "3128");
+        String baseUrl = "https://api.telegram.org/bot5297651309:AAGpziE4Q27UrK77k51FwDkDZ_h7kdbheg0/sendMessage?chat_id=-1001733208631&text=" + "yaşıyıram relax";
+        URI uri = new URI(baseUrl);
+        ResponseEntity<String> result = restTemplate.postForEntity(uri, null, String.class);
         return "Alive";
     }
+
 
 }
